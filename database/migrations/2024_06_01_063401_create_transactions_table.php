95c29122977783b21bbf2834b09c446ff4d3d7c8
Md. Raduan Islam <raduan.cse@gmail.com>
2024-06-01 16:41:22 +0600

    final commit-done

--- database/migrations/2024_06_01_063401_create_transactions_table.php
+++ database/migrations/2024_06_01_063401_create_transactions_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->enum('account_type', ['Individual', 'Business']);
-            $table->double('balance', 15, 2)->default(0);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('transaction_type', ['deposit', 'withdrawal']);
+            $table->double('amount', 15, 2);
+            $table->decimal('fee', 15, 2)->default(0);
+            $table->date('date');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::resource('users', UserController::class);
+
+Route::post('/users', [UserController::class, 'store']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::get('/transactions', [UserController::class, 'index']);
+Route::get('/show-deposits', [TransactionController::class, 'showDeposits'])->name('deposits')->middleware('auth');
+Route::get('/showDepositForm', [TransactionController::class, 'showDepositForm'])->name('deposit')->middleware('auth');
+Route::post('/deposit', [TransactionController::class, 'deposit'])->name('deposit')->middleware('auth');
+Route::get('/show-withdrawals', [TransactionController::class, 'showWithdrawals'])->name('withdrawals.index');
+Route::post('/withdrawal', [TransactionController::class, 'withdraw']);
+
+
+require __DIR__.'/auth.php';
